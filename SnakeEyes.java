64cb9614aa4ed91a859d95a7ba488bad2c03962d
Evan Varozza <VarozzaEJ@gmail.com>
2025-04-02 13:19:18 -0600

    Init

---
+++ SnakeEyes.java
@@ -0,0 +1,42 @@
+//********************************************************************
+//  SnakeEyes.java       Java Foundations
+//
+//  Demonstrates the use of a programmer-defined class.
+//********************************************************************
+
+import java.util.Scanner;
+
+public class SnakeEyes {
+	//-----------------------------------------------------------------
+	//  Creates two Die objects and rolls them several times, counting
+	//  the number of snake eyes that occur.
+	//-----------------------------------------------------------------
+	public static void main (String[] args) {
+		final int ROLLS = 500;
+		int num1, num2, count = 0, numSides;
+
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("How many sides do you want each die to have?");
+		numSides = scanner.nextInt();
+
+		Die die1 = new Die(numSides);
+		Die die2 = new Die(numSides);
+
+		for (int roll = 1; roll <= ROLLS; roll++) {
+			num1 = die1.roll();
+			num2 = die2.roll();
+
+			if (num1 == 1 && num2 == 1) {  // check for snake eyes
+				count++;
+			} 
+		}
+
+		System.out.println ("Number of rolls: " + ROLLS);
+		System.out.println ("Number of snake eyes: " + count);
+		System.out.println ("Ratio: " + (float)count / ROLLS);
+
+		scanner.close();
+	}
+}
